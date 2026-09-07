5dc96ef12786e80863e6e05ebd5d69d8659095d2
Felipe de Oliveira de Souza <felipedos@gmail.com>
2026-09-07 15:05:36 -0300

    feat: tornando o chat global

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'favela-shopping-v3.1';
+const CACHE_NAME = 'favela-shopping-v3.2';
 
 const STATIC_ASSETS = [
   '/',
--- src/app/layouts/RootLayout.tsx
+++ src/app/layouts/RootLayout.tsx
@@ -1,14 +1,24 @@
 import { Outlet } from 'react-router';
+
 import { AuthProvider } from '../../contexts/AuthContext';
+import { ChatProvider } from '../../contexts/ChatContext';
+
 import PWAHead from '../components/PWAHead';
 import PWAInstaller from '../components/PWAInstaller';
+import ChatNotification from '../components/chat/ChatNotification';
 
 export default function RootLayout() {
   return (
     <AuthProvider>
-      <PWAHead />
-      <Outlet />
-      <PWAInstaller />
+      <ChatProvider>
+        <PWAHead />
+
+        <Outlet />
+
+        <ChatNotification />
+
+        <PWAInstaller />
+      </ChatProvider>
     </AuthProvider>
   );
 }
